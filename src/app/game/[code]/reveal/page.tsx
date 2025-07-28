c4cbf54b8fdc125e1c1d3df4c81413ae68df3772
Yeonjae Lee <lyeonj@sookmyung.ac.kr>
2025-07-28 13:56:31 +0900

    chore: 불필요한 파일 삭제

---
+++ src/app/game/[code]/reveal/page.tsx
@@ -0,0 +1,21 @@
+// src/app/game/[code]/reveal/page.tsx
+'use client'
+
+import React from 'react';
+import styled from 'styled-components';
+
+const RevealPage = () => {
+    return (
+        <Wrapper>Reveal</Wrapper>
+    );
+};
+
+export default RevealPage;
+
+const Wrapper = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
