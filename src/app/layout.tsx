547ef254df3b7491d513141d4a9cc1855c933e41
Yeonjae Lee <lyeonj@sookmyung.ac.kr>
2025-07-31 02:28:17 +0900

    chore: BackgroundMusic import 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import BackgroundMusic from "@/components/BackgroundMusic";
+//import BackgroundMusic from "@/components/BackgroundMusic";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
