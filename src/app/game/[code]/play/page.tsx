ffcd196ab278a102a23b6fc7e68fe9ae5af56511
Yeonjae Lee <lyeonj@sookmyung.ac.kr>
2025-07-31 20:28:42 +0900

    Revert "chore: vercel 배포 중"
    This reverts commit 1e713d18436692d0354dc6624bc18305df2c512c.

--- src/app/game/[code]/play/page.tsx
+++ src/app/game/[code]/play/page.tsx
@@ -287,7 +287,7 @@ export default function GamePlayPage() {
   useEffect(() => {
     if (!gameId || !participants.length) return;
 
-    const gameSocket = new WebSocket(`wss://${process.env.NEXT_PUBLIC_AWS_SERVER_IP}:8081`);
+    const gameSocket = new WebSocket(`ws://${process.env.NEXT_PUBLIC_AWS_SERVER_IP}:8081`);
     // const gameSocket = new WebSocket('ws://localhost:8081');
     gameSocketRef.current = gameSocket;
 
